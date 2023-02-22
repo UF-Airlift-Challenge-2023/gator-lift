ba82909976870510247b19a173772682405f0022
Devin Willis <gatordevin@gmail.com>
2023-02-22 14:05:24 -0500

    Added support for cargo data parsing

--- mysolution.py
+++ mysolution.py
@@ -18,8 +18,29 @@ class MySolution(Solution):
     def reset(self, obs, observation_spaces=None, action_spaces=None, seed=None):
         # Currently, the evaluator will NOT pass in an observation space or action space (they will be set to None)
         super().reset(obs, observation_spaces, action_spaces, seed)
-        state = self.get_state(obs)
+        # Create an action helper using our random number generator
+        self._action_helper = ActionHelper(self._np_random)
 
+    def policies(self, obs, dones):
+        self.process_state(obs)
+        # Use the acion helper to generate an action
+        return self._action_helper.sample_valid_actions(obs)
+
+    def process_state(self, obs):
+        state = self.get_state(obs)
+        task_locations = [0]
+        delivery_pairs = []
+        demand = [0]
+        task_time_windows = [[0,100000]]
+        for cargo in state["active_cargo"]:
+            location = len(task_locations)
+            task_locations.append(cargo.location)
+            destination = len(task_locations)
+            task_locations.append(cargo.destination)
+            delivery_pairs.append([location,destination])
+            task_time_windows.append([cargo.earliest_pickup_time, cargo.hard_deadline])
+            task_time_windows.append([cargo.earliest_pickup_time, cargo.hard_deadline])
+            demand.append(cargo.weight)
         self.cargo_assignments = {a: None for a in self.agents}
         self.path = {a: None for a in self.agents}
         self.whole_path = {a: None for a in self.agents}
@@ -29,37 +50,17 @@ class MySolution(Solution):
         for plane_type in state["plane_types"]:
             self.waypoints[plane_type.id] = {"edges": [], "offsets": [], "weights": []}
 
-        for airplane_type in range(len(state["route_map"])):
-            self.multidigraph = state["route_map"][airplane_type]
-            self.waypoints[airplane_type]["edges"] = list(edge[1]-1 for edge in list(self.multidigraph.edges))
-            
-            current_node = 0
-            offsets = [0]
-            for idx, edge in enumerate(list(self.multidigraph.edges)):
-                if edge[0]-1 != current_node:
-                    current_node = edge[0]-1
-                    offsets.append(idx)
-
-            offsets.append(len(self.waypoints[airplane_type]["edges"]))
-            self.waypoints[airplane_type]["offsets"] = offsets
-            
-            weights = []
-            for idx, node_one in enumerate(list(dict(self.multidigraph.adj).keys())):
-                for node_two in list(dict(self.multidigraph.adj)[node_one].keys()):
-                    print(self.multidigraph.adj[node_one][node_two])
-                    weights.append(self.multidigraph.adj[node_one][node_two]["time"])
-            self.waypoints[airplane_type]["weights"] = weights
-
-            # w_matrix = routing.WaypointMatrix(offsets, edges, weights)
-
-        self.waypoint_graph = {"waypoint_graph":{}}
-        self.waypoint_graph["waypoint_graph"] = self.waypoints
-        print(self.waypoint_graph)
-        
-        # Create an action helper using our random number generator
-        self._action_helper = ActionHelper(self._np_random)
-
-    def policies(self, obs, dones):
-        # Use the acion helper to generate an action
-        return self._action_helper.sample_valid_actions(obs)
+        nodes = list(dict(self.multidigraph.adj).keys())
+        for node in nodes:
+            connections = list(dict(self.multidigraph.adj[node]).keys())
+            for plane_type_id in list(self.waypoints.keys()):
+                self.waypoints[plane_type_id]["offsets"].append(len(self.waypoints[plane_type_id]["edges"]))
+            for connection in connections:
+                conn_by_plane_type = self.multidigraph.adj[node][connection]
+                for plane_type_id in list(self.waypoints.keys()):
+                    if(plane_type_id in conn_by_plane_type):
+                        if(self.multidigraph.adj[node][connection][plane_type_id]["route_available"]):
+                            self.waypoints[plane_type_id]["edges"].append(connection-1)
+                            self.waypoints[plane_type_id]["weights"].append(self.multidigraph.adj[node][connection][plane_type_id]["time"])
+        return state
 
