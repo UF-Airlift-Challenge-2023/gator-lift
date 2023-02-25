5a379b75852fd4732dbe1a2a31cbb19a3438127a
Devin Willis <gatordevin@gmail.com>
2023-02-25 12:06:52 -0500

    Cuopt runs without errors now

--- mysolution.py
+++ mysolution.py
@@ -1,6 +1,7 @@
 from airlift.solutions import Solution
 from airlift.envs import ActionHelper
 from airlift.envs.airlift_env import ObservationHelper as oh, ActionHelper, NOAIRPORT_ID
+from airlift.envs import PlaneState
 import networkx as nx
 from time import sleep
 import matplotlib.pyplot as plt
@@ -47,28 +48,39 @@ class MySolution(Solution):
         super().reset(obs, observation_spaces, action_spaces, seed)
         # Create an action helper using our random number generator
         self._action_helper = ActionHelper(self._np_random)
+        self.new_change = True
 
     def policies(self, obs, dones):
-        self.process_state(obs)
+        if(self.new_change):
+            actions = self.process_state(obs)
+            self.new_change = False
         # Use the acion helper to generate an action
-        return self._action_helper.sample_valid_actions(obs)
+        # return None
+        action = self._action_helper.sample_valid_actions(obs)
+        # 'a_0' : {'process': 0, 'cargo_to_load': [], 'cargo_to_unload': [], 'destination': 0}
+        return action
 
     def process_state(self, obs):
         state = self.get_state(obs)
         task_locations = []#[0]
         delivery_pairs = []
         demand = []#[0]
+        task_ids = []
         task_time_windows = []#[[0,100000]]
         for cargo in state["active_cargo"]:
-            location = len(task_locations)
-            task_locations.append(cargo.location-1)
-            destination = len(task_locations)
-            task_locations.append(cargo.destination-1)
-            delivery_pairs.append([location,destination])
-            task_time_windows.append([cargo.earliest_pickup_time, cargo.hard_deadline])
-            task_time_windows.append([cargo.earliest_pickup_time, cargo.hard_deadline])
-            demand.append(cargo.weight)
-            demand.append(-cargo.weight)
+            if(cargo.is_available):
+                if(cargo.location!=0):
+                    location = len(task_locations)
+                    task_locations.append(cargo.location-1)
+                    destination = len(task_locations)
+                    task_locations.append(cargo.destination-1)
+                    delivery_pairs.append([location,destination])
+                    task_time_windows.append([cargo.earliest_pickup_time, cargo.hard_deadline])
+                    task_time_windows.append([cargo.earliest_pickup_time, cargo.hard_deadline])
+                    demand.append(cargo.weight)
+                    demand.append(-cargo.weight)
+                    task_ids.append(cargo.id)
+                    task_ids.append(cargo.id)
         
         self.cargo_assignments = {a: None for a in self.agents}
         self.path = {a: None for a in self.agents}
@@ -76,28 +88,21 @@ class MySolution(Solution):
         self.multidigraph = oh.get_multidigraph(state)
         self.plane_type_waypoints = {}
    
-        self.fleet_data = {"capacities":[],"vehicle_locations":[],"vehicle_types": [], "drop_return_trips" : []}
+        self.fleet_data = {"capacities":[],"vehicle_locations":[],"vehicle_types": [], "vehicle_ids":[], "drop_return_trips" : []}
 
         for agent in state["agents"]:
-            self.fleet_data["capacities"].append(state["agents"][agent]["max_weight"])
-            self.fleet_data["vehicle_locations"].append([state["agents"][agent]["current_airport"]-1,state["agents"][agent]["destination"]])
-            self.fleet_data["vehicle_types"].append(state["agents"][agent]["plane_type"])
+            if(state["agents"][agent]["state"] in [PlaneState.READY_FOR_TAKEOFF, PlaneState.WAITING]):
+                self.fleet_data["capacities"].append(state["agents"][agent]["max_weight"])
+                self.fleet_data["vehicle_locations"].append([state["agents"][agent]["current_airport"]-1,state["agents"][agent]["destination"]])
+                self.fleet_data["vehicle_types"].append(state["agents"][agent]["plane_type"])
+                self.fleet_data["vehicle_ids"].append(agent)
+                self.fleet_data["drop_return_trips"].append(True)
 
         # sort dictionary based on vehicle types
         self.fleet_data["vehicle_types"], self.fleet_data["vehicle_locations"], self.fleet_data["capacities"] = zip(*sorted(zip(self.fleet_data["vehicle_types"], self.fleet_data["vehicle_locations"], self.fleet_data["capacities"])))
         self.fleet_data["capacities"] = [self.fleet_data["capacities"]]
         self.fleet_data["min_vehicles"] = 3
-            # self.fleet_data["drop_return_trips"].append(True)
-            # self.fleet_data["vehicle_locations"] = [[1, 0], [1, 0], [1, 0]]
-            # self.fleet_data["capacities"] = [[15, 10, 10]]
-            # self.fleet_data["vehicle_types"] = [0,0,1]
-            # "capacities": [[10, 12, 15, 8, 10]],
-            # "vehicle_time_windows": [[0, 80], [1, 40], [3, 30], [5, 80], [20, 100]],
-            # "vehicle_types": [0,0,0,0,1]
-            
 
-        # nx.draw_networkx(self.multidigraph, with_labels = True)
-        # plt.show()
         self.waypoints = {}
         for plane_type in state["plane_types"]:
             self.waypoints[plane_type.id] = {"edges": [], "offsets": [], "weights": []}
@@ -125,10 +130,15 @@ class MySolution(Solution):
             "task_locations": task_locations,
             "demand": [demand],
             "task_time_windows": task_time_windows,
-            "delivery_pairs": delivery_pairs
+            "delivery_pairs": delivery_pairs,
+            "task_ids": task_ids,
         }
 
         self.fleet_data = self.fleet_data
+
+        print(self.waypoint_graph)
+        print(self.task_data)
+        print(self.fleet_data)
 
         matrix_response = requests.post(
             url + "set_cost_waypoint_graph", params=data_params, json=self.waypoint_graph
@@ -147,7 +157,7 @@ class MySolution(Solution):
         print(f"TASK ENDPOINT RESPONSE: {task_response.json()}\n")
 
 
-        solver_config = {"time_limit": 1, "number_of_climbers": 128}
+        solver_config = {"time_limit": 10, "number_of_climbers": 128}
 
         solver_config_response = requests.post(
             url + "set_solver_config", params=data_params, json=solver_config
@@ -165,37 +175,6 @@ class MySolution(Solution):
             "return_status": False,
             "return_data_state": False,
         }
-
-        # fleet_data = {
-        #     "vehicle_locations": [[0, 0], [1, 1], [0, 1], [1, 0], [0, 0]],
-        #     "capacities": [[10, 12, 15, 8, 10]],
-        #     "vehicle_time_windows": [[0, 80], [1, 40], [3, 30], [5, 80], [20, 100]],
-        #     "vehicle_types": [0,0,0,0,1]
-        # }
-
-        # fleet_response = requests.post(
-        #     url + "set_fleet_data", params=data_params, json=fleet_data
-        # )
-        # print(f"FLEET ENDPOINT RESPONSE: {fleet_response.json()}\n")
-
-        # task_data = {
-        #     "task_locations": [0, 1, 3, 4, 6, 8],
-        #     "demand": [[0, 3, 4, 4, 3, 2]],
-        #     "task_time_windows": [
-        #         [0, 1000],
-        #         [3, 20],
-        #         [5, 30],
-        #         [1, 20],
-        #         [4, 40],
-        #         [0, 30],
-        #     ],
-        #     "service_times": [0, 3, 1, 8, 4, 0],
-        # }
-
-        # task_response = requests.post(
-        #     url + "set_task_data", params=data_params, json=task_data
-        # )
-
 
         solver_response = requests.get(
             url + "get_optimized_routes", params=solve_parameters, timeout=30
